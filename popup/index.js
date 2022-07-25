718e9d38ad3737cbdbed804d59d1befdcc9dd6f3
Lauri Kajakko <kajakko.lauri@gmail.com>
2022-07-25 22:35:30 +0300

    PoC of saving and opening tabs using local storage

---
+++ popup/index.js
@@ -0,0 +1,24 @@
+const { storage, tabs } = browser;
+
+const save = async () => {
+  const allTabs = await tabs.query({});
+  storage.local.set({
+    allTabs: allTabs,
+  });
+};
+
+const retrieve = async () => {
+  const { allTabs } = await storage.local.get();
+  allTabs.forEach((element) => {
+    tabs.create({
+      url: element.url,
+    });
+  });
+};
+
+(() => {
+  document.addEventListener("click", (e) => {
+    const [, action] = e.target.classList;
+    ({ save, retrieve }[action]());
+  });
+})();
